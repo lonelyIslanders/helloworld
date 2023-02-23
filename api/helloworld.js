5983d1cf5f1337f984b01c032a20788097d19976
pengju <pengjuandu@gmail.com>
2023-02-23 12:30:59 +0800

    删除api

--- api/helloworld.js
+++
@@ -1,6 +0,0 @@
-const Koa = require('koa');
-const app = new Koa();
-app.use(async ctx => {
-    const p = Date.now();
-    ctx.body = 'Hello World' + '\n' + p;
-})
--- index.js
+++ index.js
@@ -18,6 +18,4 @@ app.use(async ctx => {
 // app.get('/about', (req, res) => {
 //     res.send('This is my about route..... ')
 // })
-
-// Export the Express API
 module.exports = app
