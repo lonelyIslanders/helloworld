ff19d0a11e46ad6409442338c102fdd137ad01f6
pengju <pengjuandu@gmail.com>
2023-02-23 12:08:27 +0800

    nb

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-const Koa = require('koa');
-const app = new Koa();
+const express = require('express')
 
-app.use(async ctx => {
-    const p = Date.now();
-    ctx.body = 'Hello World' + '\n\n' + p
+const app = express()
+const PORT = 4000
+
+app.listen(PORT, () => {
+    console.log(`API listening on PORT ${PORT} `)
 })
 
+app.get('/', (req, res) => {
+    res.send('Hey this is my API running 🥳')
+})
 
-app.listen(30000)
+app.get('/about', (req, res) => {
+    res.send('This is my about route..... ')
+})
+
+// Export the Express API
+module.exports = app
