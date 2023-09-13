fdc996e832b29df93d4b62d86ecb7ec464da564c
pengju <pengjuandu@gmail.com>
2023-09-13 17:38:23 +0800

    aa

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 const express = require('express')
 const ethers = require('ethers')
 
-const cors = require('cors')
+// const cors = require('cors')
 
 const app = express()
 
-app.use(cors())
+// app.use(cors())
 
 
 app.get('/', (req, res) => {
