94dcef96987f0c2e2052be68d2e40fbb84e5a42c
pengju <pengjuandu@gmail.com>
2023-11-02 10:04:50 +0800

    新增波场地址判断

--- index.js
+++ index.js
@@ -1,22 +1,29 @@
 const express = require('express')
 const ethers = require('ethers')
-
-// const cors = require('cors')
+const TronWeb = require('tronweb')
 
 const app = express()
 
-// app.use(cors())
 
 
 app.get('/', (req, res) => {
     const address = req.query.address;
     if (address) {
-        res.send({ code: 0, msg: ethers.utils.isAddress(address) })
+        const beg = address.substring(0, 1)
+        if (beg == '0') {
+            res.send({ code: 0, msg: ethers.utils.isAddress(address) });
+            return
+        }
+        if (beg == 'T') {
+            res.send({ code: 0, msg: TronWeb.isAddress(address) })
+            return
+        }
+        res.send({ code: 0, msg: '请传入一个有效地址' })
     } else {
-        res.send({ code: 400, msg: "请携带address！" })
+        res.send({ code: 400, msg: "请携带address" })
     }
 })
 
 app.listen(30000, () => {
-    console.log('run at 3000')
+    console.log('run at 30000')
 })
